80f0338df75066288bb60e3e4e5e3693e5457447
Michael Beers <BeersC8S@gmail.com>
2017-10-25 14:16:19 -0400

    WeatherDAL and Model noTests

---
+++ m3-w9d3-csharp-capstone/Capstone.Web/Models/Weather.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class Weather
+    {
+        public string ParkCode { get; set; }
+        public int FiveDayForcastValue { get; set; }
+        public int Low { get; set; }
+        public int High { get; set; }
+        public string Forecast { get; set; }
+        public string Recommendation { get; set; }
+
+        public int ToCelsius(int temp)
+        {
+            int tempCelsius;
+            tempCelsius = (int)Math.Round((temp - 32) / 1.8);
+            return tempCelsius;
+        }
+    }
+}
