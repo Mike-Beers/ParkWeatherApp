dc8217b689fcf9fd5fbdcc178c003e4534b34168
Michael Beers <BeersC8S@gmail.com>
2017-10-26 17:45:31 -0400

    commit to pull

---
+++ m3-w9d3-csharp-capstone/Capstone.Web.Tests/WeatherTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Capstone.Web.Tests
+{
+    [TestClass]
+    public class WeatherTests
+    {
+        [TestMethod]
+        public void ToCelsiusTest()
+        {
+
+        }
+    }
+}
